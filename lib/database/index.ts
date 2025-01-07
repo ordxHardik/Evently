0d1d04a528629b626a7d5370e1d561777d95d737
Hardik Jain <jainhardik750@gmail.com>
2025-01-07 21:40:58 +0530

    added databse

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,7 +1,6 @@
 import mongoose from "mongoose"
 
-
-
+const MONGODB_URI = process.env.MONGODB_URI;
 
 let cached = (global as any).mongoose || { conn: null, promise: null };
 
@@ -14,3 +13,8 @@ export const connectToDatabase = async () => {
       dbName: 'evently',
       bufferCommands: false,
     })
+  
+    cached.conn = await cached.promise;
+  
+    return cached.conn;
+  }
