f99d3263be278e8000062416723836d0ce1727ab
Hardik Jain <jainhardik750@gmail.com>
2025-01-09 13:06:42 +0530

    first commit of the year

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,16 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware } from '@clerk/nextjs/server';
 
 export default clerkMiddleware({
-  publicRoutes: [
-    '/',               // Publicly accessible route
-    '/events/:id',     // Publicly accessible route with dynamic parameter
-  ],
-  ignoredRoutes: [
-    '/api/webhook/clerk',   // Completely ignored by Clerk
-    '/api/webhook/stripe',  // Completely ignored by Clerk
-    '/api/uploadthing',     // Completely ignored by Clerk
-  ]
+  // publicRoutes: [
+  //   '/',              // Home
+  //   '/events/*',      // Matches /events/<any-id>
+  //   '/api/webhook/clerk',
+  //   '/api/webhook/stripe',
+  //   '/api/uploadthing'
+  // ]
+  
 });
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
